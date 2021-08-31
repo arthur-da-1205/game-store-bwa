de8aef2f5dc0966e7233d66402a9420eb1f4e088
arthur-da-1205 <arthur.darwanto@gmail.com>
2021-08-31 14:30:27 +0700

    refactoring Navbar SignIn and MainBanner

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,8 +13,10 @@ import "../styles/sidebar.css";
 import "../styles/transactions.css";
 import "../styles/transactions-detail.css";
 import "../styles/edit-profile.css";
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import "../styles/navbar-log-in.css";
+
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
